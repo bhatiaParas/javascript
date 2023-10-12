53945854a4889f66d86b694593b7145616fb51eb
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2023-10-12 04:27:02 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,11 @@
+let score="hitesh";
+console.log(typeof score);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(  valueInNumber);
+
+
+
+let isloggedIn="";
+let booleanId= Boolean(isloggedIn);
+console.log(booleanId)
