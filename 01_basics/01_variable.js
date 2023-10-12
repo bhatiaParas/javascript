4032f217e03fe2ddfff19439bf1cf7698e44b3fa
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2023-10-12 07:11:20 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId=14435
+let accountEmail="Paras@gmail.com"
+var accountPassword="12345"
+accountcity="Jaipur"
+// console.log(accountId);
+accountEmail="hc@hc.com"
+accountPassword="212121121"
+accountcity="Bengaluru"
+let accountstate;
+console.table([accountId,accountEmail,accountPassword,accountcity,accountstate])
