76a709da7f0031e620a953e66240c21844435ce1
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2023-10-12 07:50:32 +0000

    conversionoperations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,64 @@
 let score="hitesh";
-console.log(typeof score);
+// console.log(typeof score);
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(  valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(  valueInNumber);
 
 
 
 let isloggedIn="";
 let booleanId= Boolean(isloggedIn);
-console.log(booleanId)
+// console.log(booleanId)
+
+
+
+
+
+
+// *************************************OPERATIONS***********************************
+let value=3;
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%3)
+let str1="HELLO";
+let str2=" Paras";
+let str3=str1+str2;
+// console.log(str3);
+
+
+
+// console.log("1"+2);==>12
+// console.log(1+"2");==>12
+// console.log("1"+2+2);==>122
+// console.log(1+2+"2");==>32
+// console.log((3+4)*5%3);==>2
+
+
+// Pehle string dalenge to sara string ki tarah treat hoga
+// and agar string bad me dalenge to pehle number add ho jayega
+
+
+// console.log(+true); =>o/p---> 1                  NOT PREFFERABLE
+// console.log(+"");  =>o/p--->0                    NOT PREFFERABLE
+
+
+
+
+// (((((((((((((((((((((((((((((((NOT PREFFERABLE)))))))))))))))))))))))))))))))
+let num1,num2,num3;
+num1=num2=num3=2+2;
+// console.log(num1)
+// console.log(num2)
+// console.log(num3);
+                           
+let gamecounter=100;
+++gamecounter;         
+gamecounter++;         
+// *********************************PREFIX,POSTFIX**********************************
+console.log(gamecounter);
+
