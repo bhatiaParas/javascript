53e942b60dfe4b3aabc9c9c213d6f9e5770bacb4
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2023-12-29 08:35:23 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,8 @@
-const mysum = Symbol("key1")
+// singleton
+// Object.create
 
+// object literals
+const mysum = Symbol("key1")
 const Jsuser={
     name:"Paras bhatia",
     email:"paeasbhatia13630859@gmail.com",
