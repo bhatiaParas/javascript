89b7a8d6b4ce49d223ca584f4c3247d0d18247d7
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2023-12-29 08:33:40 +0000

    changes

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+const mysum = Symbol("key1")
+
+const Jsuser={
+    name:"Paras bhatia",
+    email:"paeasbhatia13630859@gmail.com",
+    "full name":"Paras Arvind Bhatia",
+    [mysum]:"mykey1"
+}
+console.log(Jsuser.email)
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+console.log(typeof [mysum]);
