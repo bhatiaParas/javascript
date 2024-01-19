ba24a7d24970cc2c7524f4b6c1b22d8545861b74
bhatiaParas <147644425+bhatiaParas@users.noreply.github.com>
2024-01-19 08:23:17 +0000

    Project 2 Solution

--- 07_projects/2-bmicalc/bmi.js
+++ 07_projects/2-bmicalc/bmi.js
@@ -1,41 +1,3 @@
-# Projects Related to DOM
-## project link
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution Code
-
-## project 1
-
-```javascript
-const buttons = document.querySelectorAll('.button');
-// console.log(buttons)
-
-const body = document.querySelector("body");
-
- buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener('click',function(e){
-        console.log(e);//e mouse event ha..humne grey color par click kiya ...--->PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-        console.log(e.target);
-        if(e.target.id ==='grey'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id ==='white'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id ==='blue'){
-            body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id ==='yellow'){
-            body.style.backgroundColor=e.target.id;
-        }
-    })
- })
-
-```
-
-## Project 2 Solution
-```javascript
 const form = document.querySelector('form');
 // this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
@@ -68,4 +30,3 @@ form.addEventListener('submit', function (e) {
   }
 });
 
-```
